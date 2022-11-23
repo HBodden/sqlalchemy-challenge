260d8e1d24bc1005fb6001c36cf7168cc972afc1
Huntley Bodden <huntley.bodden@gmail.com>
2022-11-22 19:23:37 -0500

    final HW submit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sqlalchemy-challenge
 Homework 10 repo
+in this project we are planning a trip to Honolulu, Hawaii and prior to departure we are conducting 
+a weather analysis for different times of the year. 
+
+For this homework the repo contains 
+app.py file which contains the Flask calculations for temperature data
+climate_final.ipynb that contains the final Pandas work and analysis 
+
+From the analysis, the temperatures in Hawaii are mild year round with the average percipitation being .1764 in and the Max, Min, and Avg temperatures at the station with the most readings are , 85.0, 54.0, and 71.66 respectively.
+
+The resources folder contains the .csv files and the sqlite file used for both Pandas and app.py
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+import datetime as dt
 
 #set up app.py file to read in the climate information
 #Database set up
@@ -27,8 +28,8 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-# @TODO: Complete the routes for your app here
-# YOUR CODE GOES HERE
+# Homepage.
+#List all available routes.
 @app.route("/")
 def Welcome():
     return(
@@ -38,10 +39,12 @@ def Welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>"
     )
 
-# Pretty print the data
+# Convert the query results to a dictionary using date as the key and prcp as the value.
+#Return the JSON representation of your dictionary.
 @app.route("/api/v1.0/precipitation")
 def precipitation():
    # Create session (link) from Python to the DB
@@ -64,7 +67,8 @@ def precipitation():
         all_data.append(prec_dict)
 
     return jsonify(all_data)
-    
+
+#Return a JSON list of stations from the dataset.
 @app.route("/api/v1.0/stations")
 def stations():
      # Create session from Python to the DB
@@ -83,13 +87,66 @@ def stations():
 
     return jsonify(all_names)
 
+#Query the dates and temperature observations of the most active station 
+#for the previous year of data.
+#Return a JSON list of temperature observations (TOBS) for the previous year.
+
 @app.route("/api/v1.0/tobs")
 def tobs():
+    # Create session from Python to the DB
+    session = Session(engine)
+
+    date_calc =  dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    """Return a list of all dates and precipitation measurement values"""
+    results = session.query(Measurement.prcp).filter(Measurement.date > date_calc).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    #The ravel function flattens an array(list of lists into one list)
+    #from [[1],[2],[3]] to [1, 2, 3]
+    all_measurements = list(np.ravel(results))
+
+    return jsonify(all_measurements)
+
+# Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a given start or start-end range.
+
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than or equal to the start date.
+
+@app.route("/api/v1.0/<start>")
+def by_date(start):
+    # Create session from Python to the DB
+    session = Session(engine)
     
+    """Return TMIN, TAVG, and TMAX for all dates greater than or equal to the start date."""
+   
+    results = session.query(func.max(Measurement.tobs),func.min(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+
+    session.close()
+
+   
+    #temps = [max_temp[0][0],min_temp[0][0],avg_temp[0][0]]
+    temps = list(np.ravel(results))
+      
+    return jsonify(temps)
+
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates from the start date through the end date (inclusive).
 
 @app.route("/api/v1.0/<start>/<end>")
-def temp_stats():
+def date_range(start,end):
+    # Create session from Python to the DB
+    session = Session(engine)
+    #date_calc =  dt.date.(start_dt) - dt.timedelta(days=365)
+
+    """Return the TMIN, TAVG, and TMAX for dates from the start date through the end date (inclusive)."""
+    results = session.query(func.max(Measurement.tobs),func.min(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
     
+    session.close()
+
+    all_temps = list(np.ravel(results))
+    
+    return (all_temps)
 
 #Boilerplate 
 if __name__ == "__main__":
